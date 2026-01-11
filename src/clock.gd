938bfcac881aedfa9c5a6dfa6d57732706fe1158
FaMuLan <fa_mulan@163.com>
2026-01-11 21:31:29 +0800

    更改变量名，原本是双人用的现在变成单人用

--- src/clock.gd
+++ src/clock.gd
@@ -2,36 +2,36 @@ extends Node
 
 signal timeout()
 
-var time_0:float = 0
-var start_thinking_0:float = 0
+var time_left:float = 0
+var start_thinking:float = 0
 var extra:int = 0
 var paused:bool = false
 
 func set_time(_time:float, _extra:int) -> void:
-	time_0 = _time
+	time_left = _time
 	extra = _extra
 
-func _physics_process(_delta):
+func _physics_process(_delta:float):
 	if get_time_left() <= 0:
 		end_game()
 
 func start() -> void:
-	start_thinking_0 = Time.get_unix_time_from_system()
+	start_thinking = Time.get_unix_time_from_system()
 	set_physics_process(true)
 
 func get_time_left() -> float:
 	if !paused:
-		return time_0 - (Time.get_unix_time_from_system() - start_thinking_0)
+		return time_left - (Time.get_unix_time_from_system() - start_thinking)
 	else:
-		return time_0
+		return time_left
 
 func pause() -> void:
-	time_0 -= Time.get_unix_time_from_system() - start_thinking_0
+	time_left -= Time.get_unix_time_from_system() - start_thinking
 	paused = true
 	set_physics_process(false)
 
 func resume() -> void:
-	start_thinking_0 = Time.get_unix_time_from_system()
+	start_thinking = Time.get_unix_time_from_system()
 	paused = false
 	set_physics_process(true)
 
