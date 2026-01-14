cf02399c75cb6440de4a4c0256dae24993486dc3
FaMuLan <fa_mulan@163.com>
2026-01-14 21:20:34 +0800

    将障碍物统一成同样的代码

---
+++ src/actor/barrier.gd
@@ -0,0 +1,15 @@
+extends Actor
+class_name Barrier
+
+@export var breakable:bool = false
+
+func _ready() -> void:
+	piece_type = ord('x') if breakable else ord('y')
+	var model_count:int = 0
+	for iter:Node in get_children():
+		if iter is Node3D && iter.name.is_valid_int():
+			iter.visible = false
+			model_count += 1
+	var index:int = randi() % model_count
+	var node_path:String = "%d" % index
+	get_node(node_path).visible = true
--- src/actor/shrub.gd
+++ src/actor/shrub.gd
@@ -1,8 +1,6 @@
-extends Actor
+extends Barrier
 
 func captured(_capturing:Actor = null) -> void:
-	$root.visible = false
-	$leaves.visible = false
-	$leaves_001.visible = false
+	$"0".visible = false
 	$gpu_particles_died.restart()
 	$audio_stream_player_3d.play()
