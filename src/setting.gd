bb63f031b6635f7532167570b1da1e65bb90bd01
FaMuLan <fa_mulan@163.com>
2026-01-18 01:32:24 +0800

    控制GUI控件即可更改设置

--- src/setting.gd
+++ src/setting.gd
@@ -12,11 +12,19 @@ var resolutions:Array[Vector2i] = [
 	Vector2i(3840, 2160)
 ]
 
+var languages:Dictionary[String, String] = {
+	"en": "English",
+	"zh_CN": "简体中文"
+}
+
 @onready var resolution_input:OptionButton = $texture_rect/h_box_container/v_box_container_left/margin_container_resolution/h_box_container/option_button
 @onready var fullscreen_input:CheckBox = $texture_rect/h_box_container/v_box_container_left/margin_container_fullscreen/h_box_container/check_box
 @onready var master_volume_input:HSlider = $texture_rect/h_box_container/v_box_container_left/margin_container_master_volume/v_box_container/h_slider
+@onready var master_volume_value:Label = $texture_rect/h_box_container/v_box_container_left/margin_container_master_volume/v_box_container/h_box_container/label_value
 @onready var sfx_volume_input:HSlider = $texture_rect/h_box_container/v_box_container_left/margin_container_sfx_volume/v_box_container/h_slider
+@onready var sfx_volume_value:Label = $texture_rect/h_box_container/v_box_container_left/margin_container_sfx_volume/v_box_container/h_box_container/label_value
 @onready var env_volume_input:HSlider = $texture_rect/h_box_container/v_box_container_left/margin_container_env_volume/v_box_container/h_slider
+@onready var env_volume_value:Label = $texture_rect/h_box_container/v_box_container_left/margin_container_env_volume/v_box_container/h_box_container/label_value
 @onready var language_input:OptionButton = $texture_rect/h_box_container/v_box_container_right/margin_container_language/h_box_container/option_button
 @onready var relax_input:CheckBox = $texture_rect/h_box_container/v_box_container_right/margin_container_relax/v_box_container/h_box_container/check_box
 @onready var clean_archive_input:Button = $texture_rect/h_box_container/v_box_container_right/margin_container_clean_archive/h_box_container/button
@@ -24,28 +32,53 @@ var resolutions:Array[Vector2i] = [
 func _ready() -> void:
 	for iter:Vector2i in resolutions:
 		resolution_input.add_item("%d * %d" % [iter.x, iter.y])
-	resolution_input.connect("item_selected", resolution_emitted)
-	fullscreen_input.connect("toggled", fullscreen_emitted)
-	master_volume_input.connect("value_changed", master_volume_emitted)
-	sfx_volume_input.connect("value_changed", sfx_volume_emitted)
-	env_volume_input.connect("value_changed", env_volume_emitted)
-	language_input.connect("item_selected", language_emitted)
-	relax_input.connect("toggled", relax_emitted)
-	clean_archive_input.connect("pressed", clean_archive_emitted)
+	for key:String in languages:
+		language_input.add_item(languages[key])
+	
+	resolution_input.connect("item_selected", set_resolution)
+	fullscreen_input.connect("toggled", set_fullscreen)
+	master_volume_input.connect("value_changed", set_master_volume)
+	sfx_volume_input.connect("value_changed", set_sfx_volume)
+	env_volume_input.connect("value_changed", set_env_volume)
+	language_input.connect("item_selected", set_language)
+	relax_input.connect("toggled", set_relax)
+	clean_archive_input.connect("button_down", set_clean_archive)
+	$texture_rect/button_close.connect("button_down", close)
+	visible = false
 
-func resolution_emitted(index:int) -> void:
-	pass
-func fullscreen_emitted(toggled_on:bool) -> void:
-	pass
-func master_volume_emitted(value:float) -> void:
-	pass
-func sfx_volume_emitted(value:float) -> void:
-	pass
-func env_volume_emitted(value:float) -> void:
-	pass
-func language_emitted(index:int) -> void:
-	pass
-func relax_emitted(toggled_on:bool) -> void:
-	pass
-func clean_archive_emitted() -> void:
-	pass
+func open() -> void:
+	visible = true
+
+func close() -> void:
+	visible = false
+
+func set_resolution(index:int) -> void:
+	get_window().size = resolutions[index]
+
+func set_fullscreen(toggled_on:bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func set_master_volume(value:float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index(&"Master"), value / 100.0)
+	master_volume_value.text = "%d%%" % value
+
+func set_sfx_volume(value:float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index(&"SFX"), value / 100.0)
+	sfx_volume_value.text = "%d%%" % value
+
+func set_env_volume(value:float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index(&"Ambient"), value / 100.0)
+	env_volume_value.text = "%d%%" % value
+
+func set_language(index:int) -> void:
+	TranslationServer.set_locale(languages.keys()[index])
+
+func set_relax(toggled_on:bool) -> void:
+	Progress.set_value("relax", toggled_on)
+
+func set_clean_archive() -> void:
+	if DirAccess.dir_exists_absolute("user://archive"):
+		DirAccess.remove_absolute("user://archive")
