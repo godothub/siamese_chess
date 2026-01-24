a9c6728f4e449025372f98b14e61ae4dcb9eaa1c
FaMuLan <fa_mulan@163.com>
2026-01-24 18:50:53 +0800

    遍历棋子的迭代器默认值有变动

--- src/core/state.hpp
+++ src/core/state.hpp
@@ -40,7 +40,7 @@ class State : public godot::RefCounted
 		State();
 		godot::Ref<State> duplicate();
 		void _internal_duplicate(godot::Ref<State> &other);
-		PieceIterator piece_iterator_begin(int _piece = '*');
+		PieceIterator piece_iterator_begin(int _piece = '.');
 		godot::PackedInt32Array get_all_pieces();
 		int get_piece(int _by);
 		int has_piece(int _by);
