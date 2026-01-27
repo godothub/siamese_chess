d089604b398bf1b9686cf721e433b36d14ad542f
FaMuLan <fa_mulan@163.com>
2026-01-28 01:51:34 +0800

    开启SDIFGI

--- src/level/train_carriage.gd
+++ src/level/train_carriage.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	$cheshire.play_animation("thinking")
 	$chessboard.state = Chess.create_initial_state()
 	$chessboard.add_default_piece_set()
+	$player.force_set_camera($camera_3d)
 	change_state("idle")
 
 func state_ready_idle(_arg:Dictionary) -> void:
