7e4efcdbc0d0373c7f10af31ba69bf2d7299acac
FaMuLan <fa_mulan@163.com>
2026-02-16 16:50:52 +0800

    刚获得棋子时也需要注意添加Actor对象

--- src/card_item.gd
+++ src/card_item.gd
@@ -22,18 +22,23 @@ func event() -> void:
 		ord("q"):
 			Progress.accumulate("storage_queen", 1)
 			storage_piece += 1 << (5 * 4)
+			level.chessboard.add_piece_instance_to_steady(load("res://scene/actor/piece_queen_black.tscn").instantiate().set_larger_scale(), ord("q"))
 		ord("r"):
 			Progress.accumulate("storage_rook", 1)
 			storage_piece += 1 << (6 * 4)
+			level.chessboard.add_piece_instance_to_steady(load("res://scene/actor/piece_rook_black.tscn").instantiate().set_larger_scale(), ord("r"))
 		ord("b"):
 			Progress.accumulate("storage_bishop", 1)
 			storage_piece += 1 << (7 * 4)
+			level.chessboard.add_piece_instance_to_steady(load("res://scene/actor/piece_bishop_black.tscn").instantiate().set_larger_scale(), ord("b"))
 		ord("n"):
 			Progress.accumulate("storage_knight", 1)
 			storage_piece += 1 << (8 * 4)
+			level.chessboard.add_piece_instance_to_steady(load("res://scene/actor/piece_knight_black.tscn").instantiate().set_larger_scale(), ord("n"))
 		ord("p"):
 			Progress.accumulate("storage_pawn", 1)
 			storage_piece += 1 << (9 * 4)
+			level.chessboard.add_piece_instance_to_steady(load("res://scene/actor/piece_pawn_black.tscn").instantiate().set_larger_scale(), ord("p"))
 	visible = false
 	level.chessboard.state.set_storage_piece(storage_piece)
 	Dialog.push_dialog(comment, "HINT_GET_PIECE", false, false, false)
