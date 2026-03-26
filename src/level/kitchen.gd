c75d1f2a15a378e8be400546bdba10b884344a3f
FaMuLan <fa_mulan@163.com>
2026-03-26 14:42:03 +0800

    连通咖啡厅和后厨，就差凿个洞了

---
+++ src/level/kitchen.gd
@@ -0,0 +1,11 @@
+extends Level
+
+func _ready() -> void:
+	super._ready()
+	$player.force_set_camera($camera)
+	var cheshire_by:int = get_meta("by")
+	var cheshire_instance:Actor = load("res://scene/actor/cheshire.tscn").instantiate()
+	cheshire_instance.position = $chessboard.x88_to_vector3(cheshire_by)
+	$chessboard.state.add_piece(cheshire_by, player_king)
+	$chessboard.add_piece_instance(cheshire_instance, cheshire_by)
+	chessboard.button_input_pointer = cheshire_by
