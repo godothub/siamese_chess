2c210a6da33d89846bca3af5318c38910049951f
FaMuLan <fa_mulan@163.com>
2026-07-05 16:32:07 +0800

    在康乃馨部分增加游玩说明

--- src/level/garden.gd
+++ src/level/garden.gd
@@ -19,8 +19,8 @@ func interact_carnation() -> void:
 	var target_position_2d:Vector2 = Vector2(carnation_pos.x, carnation_pos.z)
 	var target_angle:float = -current_position_2d.angle_to_point(target_position_2d) + PI / 2
 	target_angle = global_rotation.y + angle_difference(global_rotation.y, target_angle)
-	var instance:Actor = $event_explore.instance
-	instance.get_node("animation_tree").active = false
+	$event_explore.instance.get_node("animation_tree").active = false
+	$event_actor_carnation.instance.get_node("animation_tree").active = false
 	signal_container.disconnect_all()
 	signal_container.add_connection($procedure_dialog_demo.procedure_end, dialog_demo_end)
 	$procedure_dialog_demo.start()
@@ -44,7 +44,7 @@ func decision_demo_end(_result:String) -> void:
 		"":
 			interact_carnation_end()
 
-func blindfold_chess() -> void:
+func blindfold_chess(_result:String = "") -> void:
 	signal_container.disconnect_all()
 	standard_chessboard.state = Chess.create_initial_state()
 	signal_container.add_connection($procedure_decision_side.procedure_end, blindfold_decision_end)
@@ -54,6 +54,10 @@ func blindfold_decision_end(_result:String) -> void:
 	match _result:
 		"":
 			interact_carnation_end()
+			return
+		"CARNATION_TALK_DEMO_HOW_TO_PLAY":
+			signal_container.add_connection($procedure_dialog_blindfold_help.procedure_end, blindfold_chess)
+			$procedure_dialog_blindfold_help.start()
 			return
 		"SELECTION_PLAY_AS_WHITE":
 			$procedure_game.player_group = 0
@@ -68,6 +72,8 @@ func blindfold_decision_end(_result:String) -> void:
 	standard_chessboard.remove_piece_set()
 	standard_chessboard.set_enabled(true)
 	$chessboard.set_enabled(false)
+	$event_actor_carnation.instance.get_node("animation_tree").active = true
+	$event_actor_carnation.instance.play_animation("sit_and_think")
 	Player.force_set_camera($camera_chessboard)
 	signal_container.add_connection($procedure_game.procedure_end, blindfold_game_end)
 	$procedure_game.start()
@@ -76,7 +82,7 @@ func blindfold_game_end(_result:String) -> void:
 	signal_container.disconnect_all()
 	interact_carnation_end()
 
-func position_practice() -> void:
+func position_practice(_result:String = "") -> void:
 	signal_container.disconnect_all()
 	signal_container.add_connection($procedure_decision_side.procedure_end, position_practice_decision_end)
 	$procedure_decision_side.start()
@@ -88,7 +94,8 @@ func position_practice_decision_end(_result:String) -> void:
 			interact_carnation_end()
 			return
 		"CARNATION_TALK_DEMO_HOW_TO_PLAY":
-			interact_carnation_end()
+			signal_container.add_connection($procedure_dialog_positioning_help.procedure_end, position_practice)
+			$procedure_dialog_positioning_help.start()
 			return
 		"SELECTION_PLAY_AS_WHITE":
 			standard_player_group = 0
@@ -107,6 +114,8 @@ var score:int = 0
 func position_practice_start() -> void:
 	standard_chessboard.set_enabled(true)
 	$chessboard.set_enabled(false)
+	$event_actor_carnation.instance.get_node("animation_tree").active = true
+	$event_actor_carnation.instance.play_animation("sit_and_think")
 	Player.force_set_camera($camera_chessboard)
 	signal_container.add_connection(standard_chessboard.click_empty, func (_selected:int) -> void:
 		if _selected == question:
@@ -125,7 +134,7 @@ func position_practice_end() -> void:
 	signal_container.add_connection(Dialog.on_next, interact_carnation_end)
 	Dialog.push_dialog(tr("CARNATION_TALK_DEMO_POSITIONING_RESULT").format({"score": score}), "", true, true, false)
 
-func memory_practice() -> void:
+func memory_practice(_result:String = "") -> void:
 	signal_container.disconnect_all()
 	signal_container.add_connection($procedure_decision_difficulty.procedure_end, memory_practice_decision_end)
 	$procedure_decision_difficulty.start()
@@ -138,6 +147,8 @@ func memory_practice_decision_end(_result:String) -> void:
 	Player.force_set_camera($camera_chessboard)
 	standard_chessboard.set_enabled(true)
 	$chessboard.set_enabled(false)
+	$event_actor_carnation.instance.get_node("animation_tree").active = true
+	$event_actor_carnation.instance.play_animation("sit_and_think")
 	match _result:
 		"CARNATION_TALK_DEMO_MEMORIZING_EASY":
 			memory_state = Chess.create_random_state(3)
@@ -145,13 +156,17 @@ func memory_practice_decision_end(_result:String) -> void:
 			memory_state = Chess.create_random_state(6)
 		"CARNATION_TALK_DEMO_MEMORIZING_HARD":
 			memory_state = Chess.create_random_state(10)
+		"CARNATION_TALK_DEMO_HOW_TO_PLAY":
+			signal_container.add_connection($procedure_dialog_memorize_help.procedure_end, memory_practice)
+			$procedure_dialog_memorize_help.start()
+			return
 	standard_chessboard.remove_piece_set()
 	standard_chessboard.set_state(memory_state)
 	standard_chessboard.add_default_piece_set()
 	memory_answer = Chess.stringify(memory_state)
 	memory_answer = memory_answer.split(" ")[0]
-	signal_container.add_connection(Dialog.on_select, memory_practice_recite)
-	Dialog.push_selection(["CARNATION_TALK_DEMO_MEMORIZING_READY"], "CARNATION_TALK_DEMO_MEMORIZING_START", false, false)
+	signal_container.add_connection($procedure_decision_memory.procedure_end, memory_practice_recite)
+	$procedure_decision_memory.start()
 
 func memory_practice_recite(_selection:String) -> void:
 	signal_container.disconnect_all()
@@ -175,6 +190,7 @@ func interact_carnation_end() -> void:
 	signal_container.disconnect_all()
 	Player.force_set_camera($camera)
 	$event_explore.instance.get_node("animation_tree").active = true
+	$event_actor_carnation.instance.get_node("animation_tree").active = true
 	$event_explore.instance.play_animation("battle_idle")
 	$event_explore.instance.set_position($chessboard.name_to_vector3("d6"))
 	$chessboard.set_enabled(true)
