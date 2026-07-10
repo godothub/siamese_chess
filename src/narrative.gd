346aa5ccebee2e17ec0066751fc9613a07dde081
FaMuLan <fa_mulan@163.com>
2026-07-11 03:11:05 +0800

    排查一些应该播报和不应该播报的内容

--- src/narrative.gd
+++ src/narrative.gd
@@ -1,6 +1,7 @@
 extends Node
 
 func speak(content:String, interrupted:bool = false) -> void:
+	assert(content != "")
 	if Setting.get_value("text_to_speech"):
 		DisplayServer.tts_speak(content, Setting.get_value("voice"), 50, 1, 1, 0, interrupted)
 	print(content)
