727ad3356c618d9eed02cb1c4dacf5eebfa7648e
FaMuLan <fa_mulan@163.com>
2026-07-10 22:33:17 +0800

    调试输出TTS内容

--- src/narrative.gd
+++ src/narrative.gd
@@ -3,6 +3,7 @@ extends Node
 func speak(content:String, interrupted:bool = false) -> void:
 	if Setting.get_value("text_to_speech"):
 		DisplayServer.tts_speak(content, Setting.get_value("voice"), 50, 1, 1, 0, interrupted)
+	print(content)
 
 func stop() -> void:
 	DisplayServer.tts_stop()
