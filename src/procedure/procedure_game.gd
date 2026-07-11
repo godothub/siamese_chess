ab8d4f91945eb11a862756124e2996cd2a4c9ea7
FaMuLan <fa_mulan@163.com>
2026-07-11 18:31:46 +0800

    新增“更新状态”的信号，注意撤销也需要刷新引擎分析

--- src/procedure/procedure_game.gd
+++ src/procedure/procedure_game.gd
@@ -3,6 +3,7 @@ class_name ProcedureGame
 
 # 如果为2，就相当于同时控制黑白双方
 signal move_played(move:int)
+signal state_update()
 @export var player_group:int = 1
 
 var engine:ChessEngine = null	# 有可能会出现多线作战，共用同一个引擎显然不好
@@ -224,6 +225,7 @@ func state_ready_move(_arg:Dictionary) -> void:
 	|| Chess.from(_arg["move"]) == Chess.to(_arg["move"]) && !chessboard.state.has_piece(Chess.from(_arg["move"])))
 
 	history_event.push_back(chessboard.execute_move(_arg["move"]))
+	state_update.emit()
 	move_played.emit(_arg["move"])
 
 var available_events:Dictionary = {}
@@ -389,6 +391,7 @@ func take_back() -> void:
 	history_zobrist.resize(history_zobrist.size() - 2)
 	history_state.resize(history_state.size() - 2)
 	history_event.resize(history_event.size() - 2)
+	state_update.emit()
 	if history_name:
 		history_document.rollback(-1, chessboard.state, 2)
 	await chessboard.animation_finished
