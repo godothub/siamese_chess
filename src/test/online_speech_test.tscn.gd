3ad26d922ddf0ce322251be1b188f7ac7cf06617
FaMuLan <fa_mulan@163.com>
2026-07-18 17:52:53 +0800

    客户端的在线TTS和ASR测试

---
+++ src/test/online_speech_test.tscn.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@onready var tts_input:TextEdit = $margin_container/v_box_container/margin_container_tts/h_box_container/text_edit
+@onready var tts_submit:Button = $margin_container/v_box_container/margin_container_tts/h_box_container/button
+@onready var tts_request:HTTPRequest = $margin_container/v_box_container/margin_container_tts/h_box_container/http_request
+@onready var tts_result:AudioStreamPlayer = $margin_container/v_box_container/margin_container_tts/h_box_container/audio_stream_player
+@onready var asr_input:AudioStreamPlayer = $margin_container/v_box_container/margin_container_asr/h_box_container/audio_stream_player
+@onready var asr_submit:Button = $margin_container/v_box_container/margin_container_asr/h_box_container/button
+@onready var asr_request:HTTPRequest = $margin_container/v_box_container/margin_container_asr/h_box_container/http_request
+@onready var asr_result:Label = $margin_container/v_box_container/margin_container_asr/h_box_container/label
+var asr_record_effect:AudioEffectRecord = null
+
+func _ready() -> void:
+	tts_submit.connect("pressed", send_tts)
+	tts_request.set_tls_options(TLSOptions.client())
+	tts_request.connect("request_completed", receive_tts)
+	asr_submit.connect("button_down", record_asr)
+	asr_submit.connect("button_up", send_asr)
+	var record_idx = AudioServer.get_bus_index("Record")
+	asr_record_effect = AudioServer.get_bus_effect(record_idx, 0)
+
+func send_tts() -> void:
+	tts_request.request("https://127.0.0.1:5000/tts?voice=default", ["Content-Type: text/plain"], HTTPClient.METHOD_POST, tts_input.text)
+
+func receive_tts(_result:int, _response_code:int, _headers:PackedStringArray, body:PackedByteArray) -> void:
+	var audio_stream:AudioStreamMP3 = AudioStreamMP3.load_from_buffer(body)
+	if !audio_stream:
+		printerr("invalid response")
+	tts_result.stream = audio_stream
+	tts_result.play()
+
+func record_asr() -> void:
+	asr_record_effect.set_recording_active(true)
+
+func send_asr() -> void:
+	var recording:PackedByteArray = asr_record_effect.get_recording().data
+	asr_record_effect.set_recording_active(false)
+	asr_request.request_raw("https://127.0.0.1:5000/asr", ["Content-Type: audio/wav"], HTTPClient.METHOD_POST, recording)
+
+func receive_asr(_result:int, _response_code:int, _headers:PackedStringArray, body:PackedByteArray) -> void:
+	asr_result.text = body.get_string_from_utf8()
