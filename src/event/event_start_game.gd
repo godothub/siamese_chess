2b86510211d9b771eec1deef2d8a3c4ce4c03be8
FaMuLan <fa_mulan@163.com>
2026-07-21 14:23:46 +0800

    不阻塞explore中的角色移动

--- src/event/event_start_game.gd
+++ src/event/event_start_game.gd
@@ -8,8 +8,9 @@ func _ready() -> void:
 	game_event.connect("procedure_end", game_end)
 
 func on_start() -> void:
-	level.change_state("game")
+	# level.change_state("game")
 	game_event.start()
 
 func game_end(_result:String = "") -> void:
-	level.change_state("")
+	#level.change_state("")
+	pass
