cef78201ad43f11f9955535b0ed53092a5e6f736
FaMuLan <fa_mulan@163.com>
2026-07-24 20:50:14 +0800

    增加编辑焦点操作，在确认后，按下左右时不会跑到别的控件上

--- src/spinbox.gd
+++ src/spinbox.gd
@@ -13,11 +13,14 @@ signal value_changed(value:float)
 @export var font:Font = load("res://assets/fonts/FangZhengShuSongJianTi-1.ttf")
 @export var font_size:int = 28
 @export var font_color:Color = Color(1, 1, 1, 1)
+@export var font_color_selected:Color = Color(0.7, 0, 0, 1)
 @export var texture_left:Texture2D = load("res://assets/texture/spinbox_left.svg")
 @export var texture_right:Texture2D = load("res://assets/texture/spinbox_right.svg")
 @export var stylebox_normal:StyleBox = StyleBoxFlat.new()
 @export var stylebox_focus:StyleBox = StyleBoxFlat.new()
 @export var stylebox_button:StyleBox = StyleBoxFlat.new()
+
+var is_editing:bool = false
 
 func _draw() -> void:
 	var button_left_rect:Rect2 = Rect2(Vector2.ZERO, Vector2(size.y, size.y))
@@ -35,8 +38,9 @@ func _draw() -> void:
 		size / 2 - Vector2(text_lenght / 2, -text_baseline / 2),
 		text,
 		HORIZONTAL_ALIGNMENT_CENTER | VERTICAL_ALIGNMENT_CENTER,
-		-1, font_size,
-		font_color
+		-1,
+		font_size,
+		font_color_selected if is_editing else font_color
 	)
 	draw_texture_rect(texture_left, button_left_rect.grow(-10), false)
 	draw_texture_rect(texture_right, button_right_rect.grow(-10), false)
@@ -46,26 +50,43 @@ func _gui_input(event:InputEvent) -> void:
 	var button_left_rect:Rect2 = Rect2(Vector2.ZERO, Vector2(size.y, size.y))
 	var button_right_rect:Rect2 = Rect2(Vector2(size.x - size.y, 0), Vector2(size.y, size.y))
 	if has_focus():
-		if event.is_action_pressed("ui_left", true):
-			value -= step
-			value = clamp(value, range_min, range_max)
-			change_value(value)
-		if event.is_action_pressed("ui_right", true):
-			value += step
-			value = clamp(value, range_min, range_max)
-			change_value(value)
+		if is_editing:
+			if event.is_action_pressed("ui_left", true):
+				value -= step
+				value = clamp(value, range_min, range_max)
+				set_value(value)
+				get_viewport().set_input_as_handled()
+			if event.is_action_pressed("ui_right", true):
+				value += step
+				value = clamp(value, range_min, range_max)
+				set_value(value)
+				get_viewport().set_input_as_handled()
+			if event.is_action_pressed("ui_cancel") || event.is_action_pressed("ui_accept"):
+				is_editing = false
+				queue_redraw()
+				get_viewport().set_input_as_handled()
+		else:
+			if event.is_action_pressed("ui_accept", true):
+				is_editing = true
+				queue_redraw()
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
 		if button_left_rect.has_point(event.position):
 			value -= step
 			value = clamp(value, range_min, range_max)
-			change_value(value)
+			set_value(value)
 		if button_right_rect.has_point(event.position):
 			value += step
 			value = clamp(value, range_min, range_max)
-			change_value(value)
+			set_value(value)
 
-func change_value(value:float) -> void:
-	value_changed.emit(value)
+func set_value(_value:float) -> void:
+	value = _value
+	value_changed.emit(_value)
+	queue_redraw()
+
+func change_value_no_signal(_value:float) -> void:
+	value = _value
+	value_changed.emit(_value)
 	queue_redraw()
 
 func _notification(what):
