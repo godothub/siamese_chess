572302f9f0e4eef00fda565026625d7f5c6a83c2
FaMuLan <fa_mulan@163.com>
2026-08-04 19:25:13 +0800

    按“上”和"下“查看历史指令并支持修改

--- src/terminal.gd
+++ src/terminal.gd
@@ -4,11 +4,27 @@ extends CanvasLayer
 
 signal command_received(cmd:String)
 
+var limit:int = 1024
+
+var history:Array[String] = [""]
+
+var history_index:int = -1
+
 func _ready() -> void:
 	visible = false
+	$texture_rect_top/margin_container/line_edit.connect("gui_input", line_edit_input)
 	$texture_rect_top/margin_container/line_edit.connect("text_submitted", exec)
+	$texture_rect_top/margin_container/line_edit.connect("text_changed", enter)
 
 func _unhandled_input(_event:InputEvent) -> void:
+	if _event.is_action_pressed("terminal") && !visible:
+		open()
+		get_viewport().set_input_as_handled()
+	if _event.is_action_pressed("ui_cancel") && visible:
+		close()
+		get_viewport().set_input_as_handled()
+
+func line_edit_input(_event:InputEvent) -> void:
 	if _event is InputEventKey && _event.pressed:
 		if _event.is_action_pressed("terminal") && !visible:
 			open()
@@ -16,10 +32,22 @@ func _unhandled_input(_event:InputEvent) -> void:
 		if _event.is_action_pressed("ui_cancel") && visible:
 			close()
 			get_viewport().set_input_as_handled()
+		if _event.keycode == KEY_UP && visible:
+			history_index -= 1
+			history_index = clamp(history_index, 0, history.size() - 1)
+			$texture_rect_top/margin_container/line_edit.text = history[history_index]
+			get_viewport().set_input_as_handled()
+		if _event.keycode == KEY_DOWN && visible:
+			history_index += 1
+			history_index = clamp(history_index, 0, history.size() - 1)
+			$texture_rect_top/margin_container/line_edit.text = history[history_index]
+			get_viewport().set_input_as_handled()
 
 func open() -> void:
 	visible = true
+	history[-1] = ""
 	$texture_rect_top/margin_container/line_edit.text = ""
+	history_index = history.size() - 1
 	$texture_rect_top/margin_container/line_edit.grab_focus()
 
 func close() -> void:
@@ -27,4 +55,12 @@ func close() -> void:
 
 func exec(cmd:String) -> void:
 	close()
-	command_received.emit(cmd)
+	if cmd != "":
+		command_received.emit(cmd)
+		history.push_back("")
+	while history.size() > limit:
+		history.pop_front()
+
+func enter(cmd:String) -> void:
+	history[-1] = cmd
+	history_index = history.size() - 1
