4a93deed2fe22379ced3faa5ecc260e3907acec4
FaMuLan <fa_mulan@163.com>
2026-08-05 11:46:28 +0800

    使用表达式显示标题

--- src/event/event_title.gd
+++ src/event/event_title.gd
@@ -1,8 +1,21 @@
 extends LevelEvent
 class_name EventTitle
 
-@export var text:String = ""
+@export_multiline() var text:String = ""
 
-func on_init() -> void:
+var expression:Expression = Expression.new()
+
+func _ready() -> void:
+	var error:int = expression.parse(text, [])
+	if error != OK:
+		printerr(expression.get_error_text())
+		return
+
+func on_start() -> void:
+	var result:Variant = expression.execute([], self)
+	if expression.has_execute_failed():
+		printerr(expression.get_error_text())
+		return
+	var text_translated = result
 	var by:int = level.chessboard.vector3_to_x88(position)
-	level.title[by] = text
+	level.title[by] = text_translated
