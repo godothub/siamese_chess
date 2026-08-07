74a14aedf401edf688ffc5e54c3b774f4ca863d3
FaMuLan <fa_mulan@163.com>
2026-08-07 23:30:01 +0800

    删除场景上的棋局并解锁

--- src/level/basement.gd
+++ src/level/basement.gd
@@ -5,9 +5,6 @@ var light_switch:bool = false
 func _ready() -> void:
 	super._ready()
 	get_tree().call_group("lights", "set_visible", light_switch)
-	$event_teleport_computer_room.disabled = true
-	$bookshelf/bookshelf_001.position.x = 0
-	$procedure_game.connect("procedure_end", game_end)
 	light_switch = Progress.get_value("basement_light", false)
 	set_light(light_switch)
 
@@ -19,10 +16,3 @@ func change_light() -> void:
 func set_light(enabled:bool) -> void:
 	$reflection_probe.position = $reflection_probe.position + (Vector3(0, 0.0001, 0) if enabled else Vector3(0, -0.0001, 0))
 	get_tree().call_group("lights", "set_visible", enabled)
-	
-
-func game_end(result:String) -> void:
-	if result == "checkmate_black" || result == "cleared_black":
-		$event_teleport_computer_room.disabled = false
-		var tween:Tween = create_tween()
-		tween.tween_property($bookshelf/bookshelf_001, "position:x", -2, 3)
--- src/level/entrance.gd
+++ src/level/entrance.gd
@@ -4,15 +4,6 @@ func _ready() -> void:
 	super._ready()
 	Ambient.change_environment_sound(load("res://assets/audio/405135__mjeno__autumn-forest-leaves-falling-close-to-pond-iii-loopable.wav"))
 	Player.force_set_camera($camera)
-	$event_teleport_garden.disabled = true
-	$procedure_game.connect("procedure_end", game_end)
-	$elevate_fence.position.y = 0
-
-func game_end(result:String) -> void:
-	if result == "checkmate_black" || result == "cleared_black":
-		$event_teleport_garden.disabled = false
-		var tween:Tween = create_tween()
-		tween.tween_property($elevate_fence, "position:y", -1.8, 1).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 
 func inspect_door() -> void:
 	var toast:Toast = Toast.create_instance("HINT_CANT_LEAVE")
