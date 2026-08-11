f4f0a1726a8babce7a333cb7caadb7d04c2d6b73
FaMuLan <fa_mulan@163.com>
2026-08-11 12:28:25 +0800

    坐床上休息？

--- src/level/hotel_room.gd
+++ src/level/hotel_room.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	chessboard_sandbox.set_enabled(false)
 	Player.add_inspectable_item(chessboard_sandbox)
 	$procedure_game.connect("procedure_end", game_end)
+	$procedure_dialog_rest.connect("procedure_end", rest_end)
 
 func use_chessboard() -> void:
 	change_state("analysis")
@@ -23,4 +24,17 @@ func game_end(_result:String) -> void:
 	$chessboard.set_enabled(true)
 	chessboard_sandbox.set_enabled(false)
 	Player.force_set_camera($camera_bedroom)
+	change_state("")
+
+func interact_rest() -> void:
+	change_state("rest")
+	$event_explore.instance.get_node("animation_tree").active = false
+	Player.force_set_camera($procedure_dialog_rest/camera_3d)
+	$procedure_dialog_rest.start()
+
+func rest_end(_result:String) -> void:
+	Player.force_set_camera($camera_bedroom)
+	$event_explore.instance.get_node("animation_tree").active = true
+	$event_explore.instance.play_animation("battle_idle")
+	$event_explore.instance.set_position($chessboard.name_to_vector3("d5"))
 	change_state("")
