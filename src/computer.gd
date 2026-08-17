25bb5014b506aaff1b4c1b7c84ad16d8eea666b9
FaMuLan <fa_mulan@163.com>
2026-08-17 18:17:37 +0800

    在计算机互动中引入测试场景

--- src/computer.gd
+++ src/computer.gd
@@ -1,7 +1,23 @@
 extends InspectableItem
 
+var scene_instance:Node = null
+
+func _ready() -> void:
+	clear()
+
 func print(text:String) -> void:
 	$sub_viewport/texture_rect/margin_container/rich_text_label.add_text(text)
 
 func clear() -> void:
 	$sub_viewport/texture_rect/margin_container/rich_text_label.clear()
+
+func run_scene(path:String) -> void:
+	var instance:Node = load(path).instantiate()
+	scene_instance = instance
+	$sub_viewport.add_child(instance)
+	$sub_viewport/texture_rect.hide()
+
+func close_scene() -> void:
+	scene_instance.queue_free()
+	scene_instance = null
+	$sub_viewport/texture_rect.show()
--- src/level/computer_room.gd
+++ src/level/computer_room.gd
@@ -1,9 +1,64 @@
 extends Level
 
+# 至少，这台电脑可以显示“统计”、“测试”、“登录”
+var signal_container:SignalContainer = SignalContainer.new()
+
 func _ready() -> void:
 	Ambient.change_environment_sound(load("res://assets/audio/546047__grcekh__analog-crt-tv-electronic-static-noise.wav"))
 	super._ready()
 
 func interact_computer() -> void:
-	var toast:Toast = Toast.create_instance("HINT_ACCESS_DENIED")
-	add_child(toast)
+	change_state("computer")
+	$event_explore/cheshire.visible = false
+	Player.force_set_camera($camera_computer)
+	computer_main()
+
+func interact_end() -> void:
+	signal_container.disconnect_all()
+	$event_explore/cheshire.visible = true
+	Player.force_set_camera($camera_3d)
+	change_state("")
+
+func computer_main() -> void:
+	signal_container.disconnect_all()
+	signal_container.add_connection($procedure_decision_main.procedure_end, computer_main_end)
+	$computer.clear()
+	$computer.print(" ____  _                                \n/ ___|(_) __ _ _ __ ___   ___  ___  ___ \n\\___ \\| |/ _` | '_ ` _ \\ / _ \\/ __|/ _ \\\n ___) | | (_| | | | | | |  __/\\__ \\  __/\n|____/|_|\\__,_|_| |_| |_|\\___||___/\\___|\n / ___| |__   ___  ___ ___              \n| |   | '_ \\ / _ \\/ __/ __|             \n| |___| | | |  __/\\__ \\__ \\             \n \\____|_| |_|\\___||___/___/ _           \n / ___|___  _ __  ___  ___ | | ___      \n| |   / _ \\| '_ \\/ __|/ _ \\| |/ _ \\     \n| |__| (_) | | | \\__ \\ (_) | |  __/     \n \\____\\___/|_| |_|___/\\___/|_|\\___|     \n")
+	$computer.print("1." + tr("COMPUTER_ROOM_COMPUTER_STATISTICS") + " 2." + tr("COMPUTER_ROOM_COMPUTER_TEST") + " 3." + tr("COMPUTER_ROOM_COMPUTER_LOGIN"))
+	$procedure_decision_main.start()
+
+func computer_main_end(result:String) -> void:
+	match result:
+		"COMPUTER_ROOM_COMPUTER_STATISTICS":
+			computer_statistics()
+		"COMPUTER_ROOM_COMPUTER_TEST":
+			computer_test()
+		"COMPUTER_ROOM_COMPUTER_LOGIN":
+			pass
+		_:
+			interact_end()
+
+func computer_statistics() -> void:
+	pass
+
+func computer_test() -> void:
+	signal_container.disconnect_all()
+	signal_container.add_connection($procedure_decision_test.procedure_end, computer_test_decision_end)
+	$procedure_decision_test.start()
+
+func computer_test_decision_end(result:String) -> void:
+	match result:
+		"COMPUTER_ROOM_COMPUTER_TEST_ACTOR_STAGE":
+			$computer.run_scene("res://scene/test/actor_stage.tscn")
+		"COMPUTER_ROOM_COMPUTER_TEST_DECORATE_TEST":
+			$computer.run_scene("res://scene/test/decorate_test.tscn")
+		"COMPUTER_ROOM_COMPUTER_TEST_ENGINE_PLAY":
+			$computer.run_scene("res://scene/test/engine_play.tscn")
+		"COMPUTER_ROOM_COMPUTER_TEST_LICHESS_PUZZLE_TEST":
+			$computer.run_scene("res://scene/test/lichess_puzzle_test.tscn")
+		"COMPUTER_ROOM_COMPUTER_TEST_MULTIPLAYER_TEST":
+			$computer.run_scene("res://scene/test/multiplayer_test.tscn")
+		"COMPUTER_ROOM_COMPUTER_TEST_ONLINE_SPEECH_TEST":
+			$computer.run_scene("res://scene/test/online_speech_test.tscn")
+		_:
+			interact_end()
