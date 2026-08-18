ca0990ea7063343219d5ea7cf1c14eb8946b888f
FaMuLan <fa_mulan@163.com>
2026-08-19 02:23:09 +0800

    多人游戏测试场景移植

--- src/test/multiplayer_test.gd
+++ src/test/multiplayer_test.gd
@@ -11,22 +11,46 @@ var users:Dictionary = {}
 func _ready() -> void:
 	super._ready()
 	$event_explore.connect("move_executed", send_move)
-	http_event_source.connect_to_url("http://127.0.0.1:5000/stream")
+	http_event_source.connect_to_url("https://api.famulan.uk:5001/stream")
 	http_event_source.connect("event", on_server_sent_event)
 	http_request_enter.connect("request_completed", on_request_completed_enter)
-	http_request_enter.request("http://127.0.0.1:5000/room", ["Content-Type:application/json"], HTTPClient.METHOD_GET)
+	http_request_enter.request("https://api.famulan.uk:5001/room", ["Content-Type:application/json"], HTTPClient.METHOD_GET)
 
 func _process(_delta:float) -> void:
 	http_event_source.poll()
 
+func _unhandled_input(event:InputEvent) -> void:
+	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
+		var from:Vector3 = get_viewport().get_camera_3d().project_ray_origin(event.position)
+		var to:Vector3 = get_viewport().get_camera_3d().project_ray_normal(event.position) * 200
+		$ray_cast_3d.global_position = from
+		$ray_cast_3d.target_position = to
+		$ray_cast_3d.collision_mask = 3
+		$ray_cast_3d.force_raycast_update()
+		if $ray_cast_3d.is_colliding():
+			$chessboard.area_input(self, $ray_cast_3d.get_collider(), true, event.pressed, $ray_cast_3d.get_collision_point(), $ray_cast_3d.get_collision_normal())
+	if event is InputEventMouseMotion:
+		var from:Vector3 = get_viewport().get_camera_3d().project_ray_origin(event.position)
+		var to:Vector3 = get_viewport().get_camera_3d().project_ray_normal(event.position) * 200
+		$ray_cast_3d.global_position = from
+		$ray_cast_3d.target_position = to
+		$ray_cast_3d.collision_mask = 3
+		$ray_cast_3d.force_raycast_update()
+		if $ray_cast_3d.is_colliding():
+			$chessboard.area_input(self, $ray_cast_3d.get_collider(), false, event.button_mask & MOUSE_BUTTON_MASK_LEFT, $ray_cast_3d.get_collision_point(), $ray_cast_3d.get_collision_normal())
+
 func send_move(move:int) -> void:
 	var body:String = JSON.stringify({"move": Chess.x88_to_name(Chess.to(move))})
-	var error:int = http_request_move.request("http://127.0.0.1:5000/room/move", ["Content-Type:application/json", cookies], HTTPClient.METHOD_POST, body)
+	http_request_move.cancel_request()
+	var error:int = http_request_move.request("https://api.famulan.uk:5001/room/move", ["Content-Type:application/json", cookies], HTTPClient.METHOD_POST, body)
 	await http_request_move.request_completed
 	if error != OK:
 		push_error(error_string(error))
 
 func on_request_completed_enter(_result:int, _response_code:int, _headers:PackedStringArray, _body:PackedByteArray) -> void:
+	if _response_code != 200:
+		print("Network error")
+		return
 	var regex:RegEx = RegEx.new()
 	regex.compile("client_id=(([0-9a-f\\-])*);")
 	for iter:String in _headers:
@@ -39,7 +63,7 @@ func on_request_completed_enter(_result:int, _response_code:int, _headers:Packed
 	print(body)
 	var data:Dictionary = JSON.parse_string(body)
 	for iter:String in data["users"]:
-		create_user(iter, data["users"][iter])
+		create_user(iter, Chess.name_to_x88(data["users"][iter]["by"]))
 
 func on_server_sent_event(sse:ServerSentEvent) -> void:
 	var data:Dictionary = JSON.parse_string(sse.data)
--- src/test/multiplayer_test_server_node.js
+++ src/test/multiplayer_test_server_node.js
@@ -1,12 +1,21 @@
-var express = require("express");
-var uuid = require("uuid");
+var express = require("express")
+var https = require("https")
+var fs = require("fs")
+var uuid = require("uuid")
 var cookie_parser = require("cookie-parser")
-var express_sse = require("express-sse");
-var sse = new express_sse([]);
+var express_sse = require("express-sse")
+var sse = new express_sse([])
 
-var app = express();
-app.use(express.json());
-app.use(cookie_parser());
+var app = express()
+
+var options = {
+	//链接自己的SSL证书
+	key: fs.readFileSync('/privkey.pem'),
+	cert: fs.readFileSync('/fullchain.pem')
+}
+
+app.use(express.json())
+app.use(cookie_parser())
 
 var users = {}
 
@@ -14,45 +23,35 @@ function check_user_id(req, res)
 {
 	if (!req.cookies || !req.cookies.client_id)
 	{
-		var client_id = uuid.v4();
-		res.cookie("client_id", client_id);
-		console.log(`new user ${client_id}`);
-		users[client_id] = 0
-		sse.send({message: client_id}, "new_guest");
-		return client_id;
+		var client_id = uuid.v4()
+		res.cookie("client_id", client_id)
+		console.log(`new user ${client_id}`)
+		users[client_id] = {time: Date.now(), by: "a8"}
+		sse.send({message: client_id}, "new_guest")
+		return client_id
 	}
-	return req.cookies.client_id;
+	return req.cookies.client_id
 }
 
-app.get("/stream", sse.init);
+app.get("/stream", sse.init)
 
 app.get("/room", function(req, res)
 {
-	var client_id = check_user_id(req, res);
-	res.send({current_position: users[client_id], users: users})
-});
+	var client_id = check_user_id(req, res)
+	res.send({current_position: users[client_id].by, users: users})
+})
 
 app.post("/room/move", function(req, res)
 {
-	var client_id = check_user_id(req, res);
+	var client_id = check_user_id(req, res)
+	users[client_id] = {time: Date.now(), by: req.body.move}
 	console.log(`${client_id} is moved to ${req.body.move}`)
-	sse.send({message: {user:client_id ,move:req.body.move}}, "move")
+	sse.send({message: {user:client_id, move:req.body.move}}, "move")
 	res.send("")
-});
+})
 
-app.get("/room/game", function(req, res)
-{
-	check_user_id(req, res);
-	res.send("")
-});
+setInterval(() => {
+	users = {}
+}, 3600000)
 
-
-app.get("/room/game/move", function(req, res)
-{
-	check_user_id(req, res);
-	console.log(req.body)
-	res.send("")
-});
-
-
-var server = app.listen(5000, function() {});
+https.createServer(options, app).listen(5001, function() {})
